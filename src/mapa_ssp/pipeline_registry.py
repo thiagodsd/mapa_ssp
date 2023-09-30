24a50c320ee702bd52f16165a7fea482e31ebb3d
dsthiago <thiagosd.2.18@gmail.com>
2023-09-30 08:01:36 -0300

    refactor: update dependencies via poetry and prep kedro nodes for parallel execution

--- src/mapa_ssp/pipeline_registry.py
+++ src/mapa_ssp/pipeline_registry.py
@@ -1,16 +1,37 @@
+# """Project pipelines."""
+# from __future__ import annotations
+
+# from kedro.framework.project import find_pipelines
+# from kedro.pipeline import Pipeline
+
+
+# def register_pipelines() -> dict[str, Pipeline]:
+#     """Register the project's pipelines.
+
+#     Returns:
+#         A mapping from pipeline names to ``Pipeline`` objects.
+#     """
+#     pipelines = find_pipelines()
+#     pipelines["__default__"] = sum(pipelines.values())
+#     return pipelines
+
+
 """Project pipelines."""
 from __future__ import annotations
-
+from typing import Dict
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+from mapa_ssp.pipelines import raspagem
 
 
-def register_pipelines() -> dict[str, Pipeline]:
+def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
 
     Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
+        A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    pipe_raspagem = raspagem.create_pipeline()
+
+    return {"__default__": Pipeline([
+        pipe_raspagem
+    ])}
